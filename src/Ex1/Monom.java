28b166dc19c205acbfd3a4adc0aa4c8909b838e8
tomlatin <tomlatin52@gmail.com>
2019-12-01 15:55:58 +0200

    Initial commit

---
+++ src/Ex1/Monom.java
@@ -0,0 +1,201 @@
+package Ex1;
+import java.util.Comparator;
+
+/**
+ * This class represents a simple "Monom" of shape ax^b, where a is a real number and a is an integer (summed a none negative),
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * @author Boaz
+ *
+ */
+public class Monom<STRBKJ> implements function {
+	public static final Monom ZERO = new Monom(0, 0);
+	public static final Monom MINUS1 = new Monom(-1, 0);
+	public static final double EPSILON = 0.0000001;
+	public static final Comparator<Monom> _Comp = new Monom_Comperator();
+	public boolean isValidMonom=true; //Variable whose job is to determine whether the monom is correct or not
+
+
+	public static Comparator<Monom> getComp() {
+		return _Comp;
+	}
+
+	public Monom(double a, int b) {
+		this.set_coefficient(a);
+		this.set_power(b);
+	}
+
+	public Monom(Monom ot) {
+		this(ot.get_coefficient(), ot.get_power());
+	}
+
+	public double get_coefficient() {
+		return this._coefficient;
+	}
+
+	public int get_power() {
+		return this._power;
+	}
+
+	public boolean get_isValidMonom() {
+		return this.isValidMonom;
+	}
+
+	/**
+	 * this method returns the derivative monum of this.
+	 * By multiplying the coefficient by the power and subtracting 1 by the power.
+	 * @return Monum after the derivation
+	 */
+	public Monom derivative() {
+		if (this.get_power() == 0) {
+			return getNewZeroMonom();
+		}
+		return new Monom(this.get_coefficient() * this.get_power(), this.get_power() - 1);
+	}
+
+	public double f(double x) {
+		double ans = 0;
+		double p = this.get_power();
+		ans = this.get_coefficient() * Math.pow(x, p);
+		return ans;
+	}
+
+	public boolean isZero() {
+		return this.get_coefficient() == 0;
+	}
+
+	// ***************** add your code below **********************
+
+    /**
+     * @param s String received by the user
+     * The function gets a string and the function should check if the string is a proper monum if the string is correcr,
+	 * the function creates a monum.otherwise the function throws an error.
+     * A proper monum is one of the following: a,-a,ax,-ax,x,-x,ax^b,-ax^b,x^b,-x^b
+     *Examples of bad monum are the following:7tl,4^2,(4+8),8*4,3/5,4..4,(6-7),(2+2)x^4,x^2*3,3t,3t^7,x^2*3,x*x
+     */
+	public Monom(String s)  {
+		double a;
+		int b;
+		s = s.toLowerCase();
+		try {
+			if (!s.contains("x")) {
+				a = Double.parseDouble(s);
+				b = 0;
+			} else {
+				if (!s.contains("^")) {
+					if (s.equals("x")||s.equals("+x")) {
+						a = 1;
+						b = 1;
+					} else if (s.equals("-x")) {
+						a = -1;
+						b = 1;
+					} else {
+						a = Double.parseDouble((s.substring(0, s.indexOf('x'))));
+						b = 1;
+					}
+				} else {
+					if (s.charAt(0) == 'x' || ( s.charAt(0)=='+' && s.charAt(1)=='x')) {
+						a = 1;
+						b = Integer.parseInt((s.substring(s.indexOf('x') + 2)));
+					} else if ((s.charAt(0) == '-') && (s.charAt(1) == 'x')) {
+						a = -1;
+						b = Integer.parseInt((s.substring(s.indexOf('x') + 2)));
+					} else {
+						a = Double.parseDouble((s.substring(0, s.indexOf('x'))));
+						b = Integer.parseInt((s.substring(s.indexOf('x') + 2)));
+					}
+				}
+			}
+
+			set_coefficient(a);
+			set_power(b);
+		} 
+		catch (Exception e)
+        {
+			System.out.println("ERR: the string is null or string does contain a not parsable double");
+			set_isValidMonom(false);
+		}
+	}
+
+    /**
+     *  @param m The monum added to the monum on which the function operates
+     *  The function checks whether the powers are different if it then throw an error.
+     * Otherwise the function adds coefficients to each other.
+     */
+	public void add (Monom m){
+		if (this.get_power() != m.get_power())
+			throw new RuntimeException("ERR:The power are not equal");
+		else {
+			this.set_coefficient( this.get_coefficient() + m.get_coefficient());
+		}
+	}
+
+    /**
+     * @param d The monom multiplied by the monum given to the function
+     * The function checks whether one of the coefficients of the monomers is 0 if it sets the zero monom.
+     * Otherwise, multiply the coefficients and add the powers
+     */
+	public void multipy (Monom d)
+	{
+		if(this.isZero()||d.isZero()) {
+			this.set_power(0);
+			this.set_coefficient(0);
+		}
+		else
+		{
+			this.set_coefficient(this.get_coefficient() * d.get_coefficient());
+			this.set_power(this.get_power() + d.get_power());
+		}
+	}
+
+    //****************** Auxiliary functions *****************
+
+    /**
+     * @return  String Which symbolizes the monom
+     * Prints monom as follows: ax^b
+     */
+	public String toString()
+	{
+		String ans= "";
+		ans += Double.toString(this.get_coefficient()) ;
+		if (this.get_power()==1) ans+="x";
+		else if (this.get_power()!=0) ans+="x^" + Integer.toString(this.get_power());
+		return ans;
+	}
+
+	/**
+	 * @param m Monom for comparison
+	 * @return An boolean expression that signifies whether the monomies are equal or not
+	 * The function checks whether the two monomies are equal in deflection of Epsilon
+	 */
+    public boolean equals (Monom m) {
+        double dc = this.get_coefficient() - m.get_coefficient();
+        if ( Math.abs(dc) < EPSILON && this.get_power() == m.get_power()) return true;
+        return false;
+    }
+
+
+	//****************** Private Methods and Data *****************
+
+
+	private void set_coefficient ( double a){
+		this._coefficient = a;
+	}
+
+	private void set_isValidMonom ( boolean a){
+		this.isValidMonom = a;
+	}
+	private void set_power ( int p){
+		if (p < 0) {
+			throw new RuntimeException("ERR the power of Monom should not be negative, got: " + p);
+		}
+		this._power = p;
+	}
+	private static Monom getNewZeroMonom () {
+		return new Monom(ZERO);
+	}
+	private double _coefficient;
+	private int _power;
+
+}
+
---
+++ src/Ex1/Polynom.java
@@ -0,0 +1,341 @@
+package Ex1;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ *
+ * @author Boaz
+ *
+ */
+public class Polynom implements Polynom_able{
+    public LinkedList <Monom> polinomLL;
+    /**
+     * Zero (empty polynom)
+     */
+    public Polynom()
+    {
+        polinomLL= new LinkedList<Monom>();
+    }
+    /**
+     * init a Polynom from a String such as:
+     *  {"x", "3+1.4X^3-34x", "2+2x^2+4x^5"};
+     *  A proper polynomial is the connection or subtraction of proper monomies
+     *  Incorrect input:
+     *  (3x^2+1),(3x^2+1)*(9x+11),-(7x+7x^5)
+     * @param s: is a string represents a Polynom
+     */
+    public Polynom(String s)
+    {
+        polinomLL= new LinkedList<Monom>();
+        String current = "";
+        s+="+";
+        for (int i = 0; i < s.length(); i++){
+            if ((s.charAt(i) == '+' || s.charAt(i)== '-') && current!=""){
+                Monom a = new Monom(current);
+                this.add(a);
+                current = "" + s.charAt(i);
+
+            }
+            else{
+                current+= s.charAt(i);
+            }
+        }
+    }
+
+    /**
+     * A tool with which to go over the collection we chose - Link List
+     * @return Iterator of Linked List
+     */
+    @Override
+    public Iterator<Monom> iteretor() {
+        return polinomLL.iterator();
+    }
+
+    /**
+     * @param m1 The monom that is added to a polynom
+     *The add function adds a monum to an existing polynomial as follows:
+     * First, the function checks whether a monom exists in a polynomial that has the same power as the givien monum,
+     * If so they are connected.
+     * If the function does not find an equally power, then add the monum to the polynomial using a linked list function
+     * and then the linked list is sent to the makeOrder function whose function is to sort the linked list.
+     * We note that the polynomial list necessarily will always be arranged from large to small and there will also be
+     * no repetition of the same power.
+     */
+    @Override
+    public void add(Monom m1) {
+        Iterator<Monom> iter = this.iteretor();
+        boolean flag=true;
+        while (iter.hasNext()&&flag)
+        {
+            Monom nextMonom=iter.next();
+            if(nextMonom.get_power()==m1.get_power())
+            {
+                nextMonom.add(m1);
+                this.deleteUnnecessaryZeros();
+                flag=false;
+            }
+        }
+        if(flag) {
+            polinomLL.add(m1);
+            this.makeOrder();
+
+        }
+    }
+
+    /**
+     *  makeOrder The function sorts the Linken List.
+     *  This is done by the sorting function of the Linken List and the comparator we have defined
+     */
+    public void  makeOrder( )
+    {
+        Comparator<Monom> c =new Monom_Comperator();
+        this.polinomLL.sort(c);
+        this.deleteUnnecessaryZeros();
+    }
+
+    /**
+     * The function removes unnecessary zeros from the polynomial
+     */
+    public void deleteUnnecessaryZeros()
+    {
+        boolean hasValid = false;
+        Iterator<Monom> iter = this.iteretor();
+        while (iter.hasNext())
+        {
+            Monom m = iter.next();
+            if (m.isZero() && (iter.hasNext() || hasValid))
+                iter.remove();
+            else hasValid = true;
+        }
+    }
+
+    /**
+     * @param p1 The polynom that is added to a polynom on which the function works
+     * A function that connects one polynomial with another polynomial.as follows:
+     * Go over the polynomial you want to connect and add a monom monom to the desirable polynom.
+     *This is done thanks to the function that adds monom with polynom.
+     */
+    @Override
+    public void add(Polynom_able p1) {
+        Iterator<Monom> iter =p1.iteretor();
+        while(iter.hasNext())
+        {
+            this.add (iter.next());
+        }
+    }
+    /**
+     *Deep copy of Polynom_able
+     */
+    @Override
+    public Polynom_able copy() {
+        Polynom_able toCopy=new Polynom();
+        Iterator<Monom> x = this.iteretor();
+        while(x.hasNext()){
+            Monom temp = x.next();
+            toCopy.add(new Monom(temp));
+        }
+        return toCopy;
+    }
+
+    /**
+     * @return String Which symbolizes the polynom
+     * Prints polynom as follows: ax^b+...+ax^b
+     */
+    public String toString()
+    {
+        String ans = "";
+        Iterator<Monom> iter = this.iteretor();
+        while (iter.hasNext()) {
+            Monom m = iter.next();
+            if (m.get_coefficient()>=0) ans+="+";
+            ans+=m.toString();
+        }
+        if (ans.charAt(0) == '+')ans= ans.substring(1);
+        return ans;
+    }
+
+    /**
+     * @param x The X value at the point
+     * @return double Which symbolizes the Y value
+     * The function calculates the Y value at a point
+     */
+    @Override
+    public double f(double x) {
+        double sum=0;
+        Iterator<Monom> iter =this.iteretor();
+        while (iter.hasNext())
+        {
+            Monom next=iter.next();
+            sum+=next.f(x);
+        }
+        return sum;
+    }
+
+    /**
+     * @param m1 Is the monom with which we multiply the polynom on which the function works
+     * The function multiplies a polynomial by a monom.as follows:
+     * Pass the polynomial in the form of a monom-monom and multiply every monom that belongs to the polynom in the giveing monom
+     */
+    @Override
+    public void multiply(Monom m1) {
+        Iterator<Monom> iter = this.iteretor();
+        while (iter.hasNext()) {
+            Monom m = iter.next();
+            m.multipy(m1);
+        }
+    }
+
+    /**
+     * @param p1 The polynom that subtract from the polynom on which the function works
+     * The function substract polynoms.as follows:
+     * Multiplies the polynomial P1 by -1 and then add P1 to the polynom that needs to be subtracted from P1.
+     */
+    @Override
+    public void substract(Polynom_able p1) {
+        Polynom_able toSub = p1.copy();
+        toSub.multiply(Monom.MINUS1);
+        this.add(toSub);
+        this.makeOrder();
+    }
+
+    /**
+     * @param p1 Is the polynom with which we multiply the polynom on which the function works
+     *The function multiplies a polynomial by another polynomial.as follows:
+     * Pass the polynom p1 in the form of a monom-monom and  multiply every monom that belongs to the polynom p1 in the giveing polynom
+     * Connecting the multiplication of monom in polynomial will be the desired result.
+     */
+    @Override
+    public void multiply(Polynom_able p1) {
+        Polynom ans = new Polynom();
+        Iterator<Monom> iter = p1.iteretor();
+        while (iter.hasNext()) {
+            Polynom_able i = new Polynom(this.toString());
+            Monom m = new Monom( iter.next().toString());
+            if (!m.isZero()) {
+                i.multiply(m);
+                ans.add(i);
+            }
+        }
+        ans.makeOrder();
+        this.multiply(Monom.ZERO);
+        this.add(ans);
+    }
+
+    /**
+     * @param p1 The polynomial compare to the second polynomial
+     * @return Boolean answer that says whether the polynomial is equal or not
+     *The function checks whether the polynomials are equal.as follows:
+     * Compare a monom in the first polynom to a monom in the second polynom if they are not equal then return false.
+     * This is how all monomes are checked, if all are equal return true.
+     */
+    @Override
+    public boolean equals(Polynom_able p1) {
+        boolean isEquals = true;
+        Iterator<Monom> iter1 = this.iteretor();
+        Iterator<Monom> iter2 = p1.iteretor();
+        while (iter1.hasNext() && iter2.hasNext())
+        {
+            Monom m = iter1.next();
+            Monom n = iter2.next();
+            if (!m.equals(n)) isEquals = false;
+        }
+        return isEquals;
+    }
+
+    /**
+     * @return A boolean expression that represents whether the polynomial is a zero polynomial or not
+     *The function checks whether the polynomial is a zero polynomial or not.as follows:
+     * We will go over the polynom in a monom-monom if every monom is the zero monom then the polynom
+     * is the zero polynom, because our linked list is always without repetition of power.
+     */
+    @Override
+    public boolean isZero() {
+        boolean ans = true;
+        Iterator<Monom> iter = this.iteretor();
+        while (iter.hasNext()) {
+            Monom m = iter.next();
+            if(!m.isZero()) ans=false;
+        }
+        return ans;
+    }
+
+    /**
+     * Compute a value x' (x0<=x'<=x1) for with |f(x')| < eps
+     * assuming (f(x0)*f(x1)<=0, else should throws runtimeException
+     * computes f(x') such that:
+     * 	(i) x0<=x'<=x1 &&
+     * 	(ii) |f(x')|<eps
+     * 	Set the MIDX to be the average of X0 and X1 and according to the Intermediate Value Theorem,
+     * 	we can find a close point to intersect with the X axis (a small distance from the defined epsilon)
+     * 	by substituting the value corresponding to the MIDX mark (positive or negative) and repeat the
+     * 	partitioning and comparison process until the root is found
+     * @param x0 starting point
+     * @param x1 end point
+     * @param eps>0 (positive) representing the epsilon range the solution should be within.
+     * @return an approximated value (root) for this (cont.) function
+     */
+    @Override
+    public double root(double x0, double x1, double eps) {
+        if (eps<0) throw new RuntimeException("eps need to be positive");
+        if (x0 > x1)throw new RuntimeException("x0 need to be start point and x1 end point");
+        if (this.f(x0)*this.f(x1)>0) throw new RuntimeException("they are in the same side");
+        double midx = (x0+x1)/2;
+        while (Math.abs(this.f(midx))>=eps){
+            if (this.f(x0)*this.f(midx)< 0 ) x1 = midx;
+            else x0 = midx;
+            midx = (x0+x1)/2;
+        }
+        return midx;
+    }
+
+    /**
+     * @return The polynomial which is the derivative of the polynomial for which the function rise works
+     *The function calculates the derivative of the polynomial.as follows:
+     * We will go over the polynom in a monom-monom Then every monom is derived.
+     * The interconnection of all the derived monomers will form the derived polynomial
+     */
+    @Override
+    public Polynom_able derivative() {
+        Polynom_able der = new Polynom();
+        Iterator<Monom> iter = this.iteretor();
+        while (iter.hasNext()) {
+            Monom m = iter.next();
+            der.add(m.derivative());
+        }
+        return der;
+    }
+    /**
+     * @param x0 starting pooint
+     * @param x1 end point
+     * @param eps positive step value
+     * @return the approximated area above X-axis below this function bounded in the range of [x0,x1]
+     * Compute a Riman's integral from x0 to x1 in eps steps.
+     *Verify that positive epsilon is considered trapezoidal areas
+     *starting from X0 in Epsilon steps until we reach X1 only for
+     *positive heights for these X values, the amount of these areas
+     *is the area requested
+     * for more info : https://en.wikipedia.org/wiki/Trapezoidal_rule
+     */
+    @Override
+    public double area(double x0, double x1, double eps) {
+        if (eps<0) throw new RuntimeException("eps need to be positive");
+        double area = 0 , x0eps = x0 + eps;
+        if (x0>=x1){
+            return 0;
+        }
+        while (x0<=x1){
+            if (this.f(x0)>=0 && this.f(x0eps)>=0) {
+                area+= (((this.f(x0) + this.f(x0eps)) / 2) * eps);
+            }
+            x0+=eps;
+            x0eps = x0 + eps;
+        }
+        return area;
+    }
+}
