3e1a9915205bad055ea6debb47651c464dba2c3e
tomlatin <tomlatin52@gmail.com>
2019-12-07 17:00:44 +0200

    we have the same probleme in the functions: plus,mul,div,max,min,comp. we have a probolem in the function toString-we can see a example of the problem in the test class.

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -1,51 +1,249 @@
 package Ex1;
 
 public class ComplexFunction implements complex_function {
+    private function left;
+    private function right;
+    private Operation op;
 
+    /**
+     * A definitive constructor that initializes both functions to be Null and the operator to be None
+     */
+    public ComplexFunction() {
+        this.left = null;
+        this.right = null;
+        this.op = Operation.None;
+    }
+
+    /**
+     * @param op     string that marks a mathematical operation
+     * @param fLeft  intended to be a left function
+     * @param fRight intended to be a right function
+     */
+    public ComplexFunction(String op, function fLeft, function fRight) {
+        op = op.toLowerCase();
+        switch (op) {
+
+            case "plus": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Plus;
+            }
+            break;
+
+            case "times": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Times;
+            }
+            break;
+
+            case "divid": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Divid;
+            }
+            break;
+
+            case "max": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Max;
+            }
+            break;
+
+            case "min": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Min;
+            }
+            break;
+
+            case "comp": {
+                this.left = fLeft;
+                this.right = fRight;
+                this.op = Operation.Comp;
+            }
+            break;
+
+            default:
+                System.out.println("ERR:The operation is invalid");
+                break;
+
+        }
+
+    }
+
+    /**
+     *constructor that accepts one function and puts it as the left function, the operator will be None and the right function will be null
+     * @param fLeft intended to be a left function
+     */
+    public ComplexFunction(function fLeft)
+    {
+        this.left=fLeft;
+        this.op=Operation.None;
+        this.right=null;
+    }
+
+    /**
+     * Copy constructor.
+     * @param original The ComplexFunction object that needs to be copied
+     */
+    public ComplexFunction(ComplexFunction original)
+    {
+        this.op=original.getOp();
+        this.left=original.left();
+        this.right=original.right();
+    }
 
     @Override
     public void plus(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Plus;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Plus;
+        }
     }
 
     @Override
     public void mul(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Times;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Times;
+        }
     }
 
     @Override
     public void div(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Divid;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Divid;
+        }
     }
 
     @Override
     public void max(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Max;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Max;
+        }
     }
 
     @Override
     public void min(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Min;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Min;
+        }
     }
 
     @Override
     public void comp(function f1) {
-
+        if(this.getOp()==Operation.None)
+        {
+            this.right=f1;
+            this.op=Operation.Comp;
+        }
+        else
+        {
+            this.left = this;
+            this.right = f1;
+            this.op = Operation.Comp;
+        }
     }
 
+    /**
+     * get left.
+     * @return The left function from ComplexFunction
+     */
     @Override
-    public function left() {
-        return null;
-    }
+    public function left() { return this.left;}
 
+    /**
+     * get right.
+     * @return The right function from ComplexFunction
+     */
     @Override
-    public function right() {
-        return null;
-    }
+    public function right() { return this.right; }
 
+    /**
+     * get op.
+     * @return The Operation from ComplexFunction
+     */
     @Override
     public Operation getOp() {
-        return null;
+        return this.op;
+    }
+
+    public String toString()
+    {
+        StringBuilder s=new StringBuilder();
+        s.append(this.opToString());
+        if(this.left!=null)
+        {
+            s.append("("+this.left().toString());
+        }
+        if(this.right!=null)
+        {
+            s.append(","+this.right().toString()+")");
+        }
+        return s.toString();
+    }
+    public String opToString()
+    {
+        switch (this.op)
+        {
+            case Max:
+                return "max";
+            case Min:
+                return "min";
+            case Comp:
+                return "comp";
+            case Plus:
+                return "plus";
+            case Divid:
+                return "div";
+            case Times:
+                return "mul";
+            case None:
+               return "none";
+            default:
+                return "error";
+
+        }
     }
 
     @Override
--- src/Tests/ComplexFunctionTest.java
+++ src/Tests/ComplexFunctionTest.java
@@ -1,18 +1,128 @@
 package Tests;
 
+import Ex1.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ComplexFunctionTest {
-
+    static function f0;
+    static function f1;
     @Before
-    public void setUp() throws Exception {
+    public void BeforeEach() {
+        Polynom p0=new Polynom("2x^2+4");
+        Polynom p1=new Polynom("8x^2+6");
+        f0=p0.copy();
+        f1=p1.copy();
+    }
+
+    /**
+     * Checks that the default constructor works properly, i.e. the right and left functions are equal to null and the operator is equal to None
+     */
+    @Test
+    public void DefaultComplexFunction()
+    {
+        ComplexFunction c= new ComplexFunction();
+        assertNull(c.left());
+        assertNull(c.right());
+        assertEquals("None",c.getOp().toString());
+    }
+
+    /**
+     * Checks that the constructor that accepts 2 functions and operator works properly
+     */
+    @Test
+    public void threeParametersComplexFunction()
+    {
+        ComplexFunction cPlus= new ComplexFunction("plus",f0,f1);
+        assertEquals("2.0x^2+4.0",cPlus.left().toString());
+        assertEquals("8.0x^2+6.0",cPlus.right().toString());
+        assertEquals("Plus",cPlus.getOp().toString());
+
+        ComplexFunction cTimes= new ComplexFunction("Times",f0,f1);
+        assertEquals("2.0x^2+4.0",cTimes.left().toString());
+        assertEquals("8.0x^2+6.0",cTimes.right().toString());
+        assertEquals("Times",cTimes.getOp().toString());
+
+        ComplexFunction cDivid= new ComplexFunction("Divid",f0,f1);
+        assertEquals("2.0x^2+4.0",cDivid.left().toString());
+        assertEquals("8.0x^2+6.0",cDivid.right().toString());
+        assertEquals("Divid",cDivid.getOp().toString());
+
+        ComplexFunction cMax= new ComplexFunction("Max",f0,f1);
+        assertEquals("2.0x^2+4.0",cMax.left().toString());
+        assertEquals("8.0x^2+6.0",cMax.right().toString());
+        assertEquals("Max",cMax.getOp().toString());
+
+        ComplexFunction cMins= new ComplexFunction("Min",f0,f1);
+        assertEquals("2.0x^2+4.0",cMins.left().toString());
+        assertEquals("8.0x^2+6.0",cMins.right().toString());
+        assertEquals("Min",cMins.getOp().toString());
+
+        ComplexFunction cComp= new ComplexFunction("Comp",f0,f1);
+        assertEquals("2.0x^2+4.0",cComp.left().toString());
+        assertEquals("8.0x^2+6.0",cComp.right().toString());
+        assertEquals("Comp",cComp.getOp().toString());
+    }
+
+    /**
+     * Checks that the constructor that accepts one function works properly,
+     * i.e. the function that accepts puts as the left function, the operator will be None and the right function will be null
+     */
+    @Test
+    public void oneParametersComplexFunction()
+    {
+        ComplexFunction cPlus= new ComplexFunction(f0);
+        assertEquals("2.0x^2+4.0",cPlus.left().toString());
+        assertNull(cPlus.right());
+        assertEquals("None",cPlus.getOp().toString());
+
+        ComplexFunction cTimes= new ComplexFunction(f1);
+        assertEquals("8.0x^2+6.0",cTimes.left().toString());
+        assertNull(cPlus.right());
+        assertEquals("None",cTimes.getOp().toString());
+
+    }
+
+    /**
+     * Checks that the copy constructor works properly,
+     * i.e. The constructor copies what ComplexFunction the left function the right function and the operator
+     */
+    @Test
+    public void CopyComplexFunction()
+    {
+        ComplexFunction c= new ComplexFunction();
+        ComplexFunction cPlus= new ComplexFunction("plus",f0,f1);
+        ComplexFunction cTimes= new ComplexFunction("Times",f0,f1);
+        ComplexFunction cDivid= new ComplexFunction("Divid",f0,f1);
+        ComplexFunction cMax= new ComplexFunction("Max",f0,f1);
+        ComplexFunction cMins= new ComplexFunction("Min",f0,f1);
+        ComplexFunction cComp= new ComplexFunction("Comp",f0,f1);
+
+
+        ComplexFunction[] arrC={c,cPlus,cTimes,cDivid,cMax,cMins,cComp};
+        for (int i = 0; i <arrC.length ; i++) {
+            ComplexFunction cCopy= new ComplexFunction(arrC[i]);
+            assertEquals(arrC[i].getOp(),cCopy.getOp());
+            assertEquals(arrC[i].left(),cCopy.left());
+            assertEquals(arrC[i].right(),cCopy.right());
+        }
     }
 
     @Test
     public void plus() {
+        ComplexFunction c0= new ComplexFunction(f0);
+        assertEquals("2.0x^2+4.0",c0.left().toString());
+        assertNull(c0.right());
+        assertEquals("None",c0.getOp().toString());
+
+        c0.plus(f1);
+        assertEquals("Plus",c0.getOp().toString());
+        assertNotNull(c0.left());
+        assertNotNull(c0.right());
+
+
     }
 
     @Test
@@ -57,5 +167,15 @@ public class ComplexFunctionTest {
 
     @Test
     public void copy() {
+    }
+
+
+    @Test
+    public void TtoString() {
+        ComplexFunction cPlus= new ComplexFunction("plus",f0,f1);
+        assertEquals("plus(2.0x^2+4.0,8.0x^2+6.0)",cPlus.toString());
+        cPlus.comp(f0);
+//        assertEquals("comp(plus(2.0x^2+4.0,8.0x^2+6.0),2.0x^2+4.0)",cPlus.toString());
+//        System.out.println(cPlus.toString());
     }
 }
